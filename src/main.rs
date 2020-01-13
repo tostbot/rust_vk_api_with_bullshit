da11877ef6615996529221b217cf8970636847c6
andrey <a.simaranov@gmail.com>
2020-01-13 23:27:19 +0300

    A)

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,5 @@ fn main() {
 
     let session = Session::from_token("abacaba");
     let api = VkApi::new(session);
-    api.call("users.get", hashmap!("user_ids".to_string() => Box::new(1 as VkApiArg)));
+    api.call("users.get", hashmap!("user_ids".to_string() => Box::new(1 as dyn VkApiArg)));
 }
